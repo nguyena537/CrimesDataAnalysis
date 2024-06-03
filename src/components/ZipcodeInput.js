ec22027770f2fdd4c2dd6c33b46220fd6de8b07c
Andre Nguyen <nguyena537@gmail.com>
2024-06-02 17:07:05 -0700

    fix error

--- src/components/ZipcodeInput.js
+++ src/components/ZipcodeInput.js
@@ -7,40 +7,41 @@ export default function ZipcodeInput({ city, onSubmitZipcode, loading, setLoadin
     const handleSubmit = (event) => {
         event.preventDefault();
         if (zipcode.length >= 5) {
-        setLoading(true);
-        setCrimeData([]);
-        setRaceData({});
-        setIncomeData({});
-        setCrimeTypeData([]);
-        onSubmitZipcode(zipcode);
-        axios.all([
-            axios.get(`http://127.0.0.1:3000/crimesForZipcode/${zipcode}`),
-            axios.get(`http://127.0.0.1:3000/crimeTypesForZipcode/${zipcode}`),
-            axios.get(`http://127.0.0.1:3000/dataForZipcode/${zipcode}`),
-        ])
-        .then(axios.spread((crimesRes, crimeTypesRes, zipcodeDataRes) => {
-            console.log('API responses:', { crimesRes, crimeTypesRes, zipcodeDataRes});
-            var zipcodeData = zipcodeDataRes.data;
-            setCrimeData(crimesRes.data);
-            setCrimeTypeData(crimeTypesRes.data);
-            setRaceData({
-            race_black: zipcodeData.race_black,
-            race_white: zipcodeData.race_white,
-            race_asian: zipcodeData.race_asian
-            })
-            setIncomeData({income: zipcodeData.income})
-            setLoading(false);
-        }))
-        .catch(error => {
-            console.error('Error fetching data:', error);
-            setError('There was an error fetching the data!');
-            setLoading(false);
-        });
-        } else {
-        setCrimeData([]);
-        setCrimeTypeData([]);
-        setRaceData({});
-        setIncomeData({});
+            setError("");
+            setLoading(true);
+            setCrimeData([]);
+            setRaceData({});
+            setIncomeData({});
+            setCrimeTypeData([]);
+            onSubmitZipcode(zipcode);
+            axios.all([
+                axios.get(`http://127.0.0.1:3000/crimesForZipcode/${zipcode}`),
+                axios.get(`http://127.0.0.1:3000/crimeTypesForZipcode/${zipcode}`),
+                axios.get(`http://127.0.0.1:3000/dataForZipcode/${zipcode}`),
+            ])
+            .then(axios.spread((crimesRes, crimeTypesRes, zipcodeDataRes) => {
+                console.log('API responses:', { crimesRes, crimeTypesRes, zipcodeDataRes});
+                var zipcodeData = zipcodeDataRes.data;
+                setCrimeData(crimesRes.data);
+                setCrimeTypeData(crimeTypesRes.data);
+                setRaceData({
+                race_black: zipcodeData.race_black,
+                race_white: zipcodeData.race_white,
+                race_asian: zipcodeData.race_asian
+                })
+                setIncomeData({income: zipcodeData.income})
+                setLoading(false);
+            }))
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                setError('There was an error fetching the data!');
+                setLoading(false);
+            });
+            } else {
+            setCrimeData([]);
+            setCrimeTypeData([]);
+            setRaceData({});
+            setIncomeData({});
         }
     };
 
